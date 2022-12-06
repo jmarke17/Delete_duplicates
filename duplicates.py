a20ed496f03fedeada941221870a6466bf3696f9
Juan Marquesán <87093287+jmarke17@users.noreply.github.com>
2022-12-06 19:55:51 +0100

    change striw to raw string

--- duplicates.py
+++ duplicates.py
@@ -5,7 +5,7 @@ import os
 import hashlib
 
 # Define the path of the directory where the files are located
-folder_path = "C:/my_files"
+folder_path = r"C:\Users\orden\Desktop\WDR-20221126094734"
 
 # Create a dictionary to store the hash of each file and its name
 file_hashes = {}
@@ -14,24 +14,25 @@ file_hashes = {}
 for file_name in os.listdir(folder_path):
     # Build the full path of the file
     file_path = os.path.join(folder_path, file_name)
+    try:
+        # Check if the file is a regular file (not a directory or a symbolic link)
+        if os.path.isfile(file_path):
+            # Open the file in binary reading mode
+            with open(file_path, "rb") as f:
+                # Calculate the hash of the file
+                file_hash = hashlib.sha256(f.read()).hexdigest()
 
-    # Check if the file is a regular file (not a directory or a symbolic link)
-    if os.path.isfile(file_path):
-        # Open the file in binary reading mode
-        with open(file_path, "rb") as f:
-            # Calculate the hash of the file
-            file_hash = hashlib.sha256(f.read()).hexdigest()
+            # Check if the hash of the file already exists in the dictionary
+            if file_hash in file_hashes:
+                # If the hash already exists, remove the current file
+                os.remove(file_path)
 
-        # Check if the hash of the file already exists in the dictionary
-        if file_hash in file_hashes:
-            # If the hash already exists, remove the current file
-            os.remove(file_path)
-
-            # Show a message on the screen
-            print("The file {} is a duplicate and has been deleted".format(file_name))
-        else:
-            # If the hash does not exist, add it to the dictionary along with the file name
-            file_hashes[file_hash] = file_name
-
+                # Show a message on the screen
+                print("The file {} is a duplicate and has been deleted".format(file_name))
+            else:
+                # If the hash does not exist, add it to the dictionary along with the file name
+                file_hashes[file_hash] = file_name
+    except:
+        pass
 # Show a final message
 print("All duplicate files in the directory have been deleted")
